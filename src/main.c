3aa24d6d4234d889e6c1e68107c80f0a0a4b2f98
João <soyjog@gmail.com>
2020-03-16 22:35:24 -0300

    Passou nos testes e implementado usando pilha

--- src/main.c
+++ src/main.c
@@ -6,29 +6,35 @@
 #include <stdio.h>
 #define tam_buffer 100
 
+//Struct da Pilha
 typedef struct Stack{
 	char data[tam_buffer];
 	int pos;
 } Stack;
 
+//Inicializa a pilha vazia
 void init_stack(Stack *s){
 	s->pos = 0;
 }
 
+//Adiciona elemento ao topo da pilha
 void push(Stack *s, char c){
 	if(s->pos < tam_buffer)
 		s->data[(s->pos)++] = c;
 } 
 
+//Retira e retorna o elemento do topo da pilha
 char pop(Stack *s){
 	if(s->pos >= 0)
 		return s->data[--(s->pos)];
 }
 
+//Imprime tudo o que tem na pilha
 void print_stack(Stack *s){
 	while(s->pos != 0)
 		printf("%c", pop(s));
 }
+
 
 int main() {
 	char buffer[tam_buffer];
@@ -36,6 +42,7 @@ int main() {
 	init_stack(&stack);
 	fgets(buffer, tam_buffer, stdin);
 	
+	//Loop que adiciona cada letra a pilha e quando a palavra termina imprime a pilha
 	for(int i = 0; buffer[i] != '\n'; i++){
 		if(buffer[i] == ' '){
 			print_stack(&stack);
@@ -45,6 +52,7 @@ int main() {
 			push(&stack, buffer[i]);
 	}
 
+	//imprime o que restou na pilha (ultima palavra)
 	print_stack(&stack);
 	printf("\n");
 	
